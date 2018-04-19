6b1c3a4a6aed45ac19476b82d5c9a551e688e90e
Shannon Nolan <shannonnolan17@gmail.com>
2018-04-19 13:12:46 -0700

    Initial commit

---
+++ test/system/coffee_roasts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CoffeeRoastsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit coffee_roasts_url
+  #
+  #   assert_selector "h1", text: "CoffeeRoast"
+  # end
+end
